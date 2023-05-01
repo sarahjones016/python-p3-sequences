ed31e0a6a34a015d02084a6c60535ec86212de19
Sarah Jones <sarahrjones8@gmail.com>
2023-05-01 11:44:40 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+
+    fibonacci_sequence = []
+
+    if length > 0:
+        fibonacci_sequence.append(0)
+        if length > 1:
+            fibonacci_sequence.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci_sequence.append(fibonacci_sequence[-2] + fibonacci_sequence[-1])
+
+    print(fibonacci_sequence)
+
+    
